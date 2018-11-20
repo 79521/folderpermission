996318163c5c2a97d371c5587c593c5e62193124
Fernando Keil <f.keil@hyva.com.br>
2018-11-20 13:40:51 -0200

    Clean some code

--- set-permission.ps1
+++ set-permission.ps1
@@ -3,10 +3,8 @@ $new_user = Read-Host -Prompt "domain\new-user"
 $copy_user= Read-Host -Prompt "domain\model-user"
 #set main/start folder
 $main_path = Read-Host -Prompt "\\FolderInitialPath"
-
 #Search for folder where $copy_user have permission and set to $ACL the path and access properties
 $ACL = Get-ChildItem -Path $main_path -Recurse | where-object {($_.PsIsContainer)}| Get-ACL | Where-Object AccessToString -like "*$copy_user*" | Select-Object Path,Access
-
 #crawl into $ACL
 foreach($folderpath in $acl)
 {
@@ -19,11 +17,9 @@ foreach($folderpath in $acl)
 	$ruler =  Get-Acl -path $path
 	#create $rule var to look what permissions $copy_user have
 	$rule= Get-Acl -path $path  | Select-Object -ExpandProperty Access
-	
-	#crawl into $rule
+		#crawl into $rule
 	Foreach($permission in $rule){
-	
-		#some tests into each access rule
+			#some tests into each access rule
 		If($permission.IdentityReference.Value -eq $copy_User){
 			#Create $ace var to generate new ACL equal to $copy_user permissions and set to $new_user
 			$ace = New-Object System.Security.AccessControl.FileSystemAccessRule($new_user,$permission.FileSystemRights,$permission.InheritanceFlags,$permission.PropagationFlags,$permission.AccessControlType)
